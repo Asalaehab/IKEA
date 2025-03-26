9d70bec2441ece86d2908148c15da45c3fa4a56d
asalaehab824@gmail.com <asalaehab824@gmail.com>
2025-03-26 02:10:31 +0200

    Implenment Employee DTO

--- IKEA.BLL/DTO's/Employees/EmployeeDTO.cs
+++ IKEA.BLL/DTO's/Employees/EmployeeDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IKEA.DAL.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,21 @@ namespace IKEA.BLL.DTO_s.Employees
 {
     public class EmployeeDTO
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+
+        public int? Age { get; set; }
+
+
+        public decimal Salary { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public string? Email { get; set; }
+
+
+        public string Gender { get; set; } = null!;//appear like string store like Gender
+
+        public EmployeeType EmployeeType { get; set; }
     }
 }
