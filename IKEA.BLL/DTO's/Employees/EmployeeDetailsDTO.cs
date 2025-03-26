ed2bd8f19aa3295c6f019fd5c07cb2fb2e321cd1
asalaehab824@gmail.com <asalaehab824@gmail.com>
2025-03-26 02:20:05 +0200

    implement EmployeesDTOS Create,Update,Details

--- IKEA.BLL/DTO's/Employees/EmployeeDetailsDTO.cs
+++ IKEA.BLL/DTO's/Employees/EmployeeDetailsDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IKEA.DAL.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,35 @@ namespace IKEA.BLL.DTO_s.Employees
 {
     public class EmployeeDetailsDTO
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
 
+        public int? Age { get; set; }
+
+        public string Address { get; set; } = null!;
+
+        public decimal Salary { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public DateOnly HiringDate { get; set; }
+
+        public Gender Gender { get; set; }
+
+        public EmployeeType EmployeeType { get; set; }
+
+        #region Adminstartor
+        public int createdBy { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public int LastModifiedBy { get; set; }
+
+        public DateTime LastModifiedOn { get; set; }
+        #endregion
     }
 }
--- IKEA.BLL/DTO's/Employees/UpdatedEmployeeDTO.cs
+++ IKEA.BLL/DTO's/Employees/UpdatedEmployeeDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IKEA.DAL.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,24 @@ namespace IKEA.BLL.DTO_s.Employees
 {
    public class UpdatedEmployeeDTO
     {
+        public string Name { get; set; } = null!;
+
+        public int? Age { get; set; }
+
+        public string Address { get; set; } = null!;
+
+        public decimal Salary { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public DateOnly HiringDate { get; set; }
+
+        public Gender Gender { get; set; }
+
+        public EmployeeType EmployeeType { get; set; }
     }
 }
